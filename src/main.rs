8ef9042cf99d82c1dfc7d528177989df1c361323
suitedgalaxy <suitedgalaxy63@gmail.com>
2025-10-21 16:59:50 -0400

    it's got the clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use watchlist::{MediaType, MultiPartWatchProgress, WatchPosition, WatchableMedia};
+use clap::Parser;
 
 fn main() {
+    let cli = Cli::parse();
+
     let media = WatchableMedia {
         title: "Chainsaw Man".to_owned().into_boxed_str(),
         year: Some(2022),
@@ -16,4 +19,11 @@ fn main() {
         Ok(s) => println!("{s}"),
         Err(_) => eprintln!("error"),
     }
+}
+
+
+
+#[derive(Parser)]
+struct Cli {
+    watchlist_file: String,
 }

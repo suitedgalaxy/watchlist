f63371f2fb74c8321c86ff95b47cb1548c34e7c1
suitedgalaxy <suitedgalaxy63@gmail.com>
2025-10-21 16:35:33 -0400

    structure and serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use watchlist::{MediaType, MultiPartWatchProgress, WatchPosition, WatchableMedia};
+
 fn main() {
-    println!("Hello, world!");
+    let media = WatchableMedia {
+        title: "Chainsaw Man".to_owned().into_boxed_str(),
+        year: Some(2022),
+        media_type: MediaType::Anime {
+            watch_progress: MultiPartWatchProgress::Finished(WatchPosition {
+                season: 1,
+                episode: 12,
+            }),
+            ongoing: true,
+        },
+    };
+    match ron::to_string(&media) {
+        Ok(s) => println!("{s}"),
+        Err(_) => eprintln!("error"),
+    }
 }
